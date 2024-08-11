d3f72a751f6c9f80d3ef0804c66a10bfcac019ff
Mitchell Christ <mitchell@human.mareketing>
2024-08-11 16:18:15 -0700

    add dashboard tool

--- src/sanity/sanity.config.ts
+++ src/sanity/sanity.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'sanity'
 import { projectId, dataset } from './src/env'
 import { structureTool } from 'sanity/structure'
+import {
+	dashboardTool,
+	projectInfoWidget,
+	projectUsersWidget,
+} from '@sanity/dashboard'
+// import { vercelWidget } from 'sanity-plugin-dashboard-widget-vercel'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemaTypes'
 
@@ -17,8 +23,21 @@ export default defineConfig({
 			name: 'content',
 			title: 'Content',
 		}),
+		dashboardTool({
+			name: 'deployment',
+			title: 'Deployment',
+			// widgets: [vercelWidget()],
+		}),
+		dashboardTool({
+			name: 'info',
+			title: 'Info',
+			widgets: [projectInfoWidget(), projectUsersWidget()],
+		}),
 		visionTool(),
 	],
+
+	scheduledPublishing: { enabled: false },
+	tasks: { enabled: false },
 
 	schema: {
 		types: schemaTypes,
