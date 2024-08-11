e5f026c74adb8f8b591b640fa293064fc7d2e818
Mitchell Christ <mitchell@human.mareketing>
2024-08-11 15:10:01 -0700

    init sanity

---
+++ sanity/sanity.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'sanity'
+import { projectId, dataset } from './src/env'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemaTypes'
+
+export default defineConfig({
+	name: 'default',
+	title: 'SanityPress',
+
+	projectId,
+	dataset,
+	// basePath: '/admin',
+
+	plugins: [
+		structureTool({
+			name: 'content',
+			title: 'Content',
+		}),
+		visionTool(),
+	],
+
+	schema: {
+		types: schemaTypes,
+	},
+})
---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,9 @@
+// documents
+// objects
+// modules
+
+export const schemaTypes = [
+	// documents
+	// objects
+	// modules
+]
